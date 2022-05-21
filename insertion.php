b1663cca0ef4d4149efeffc46e657f52b852aa17
sabo110 <samirphanuel@gmail.com>
2022-05-21 22:46:16 +0200

    ajout des checkbox

--- insertion.php
+++ insertion.php
@@ -1,6 +1,9 @@
 <?php
+    
     if(isset($_POST["envoyer"])){
-        if(!empty($_POST["nom"]) and !empty($_POST["prenom"]) and !empty($_POST["email"]) and !empty($_POST["mdp"]) and !empty($_POST["sexe"]) and !empty($_POST["confirmation"])){
+        if(!empty($_POST["nom"]) and !empty($_POST["prenom"]) and !empty($_POST["email"]) and !empty($_POST["mdp"]) and !empty($_POST["sexe"]) and !empty($_POST["confirmation"]) and !empty($_POST["tab"])){
+            $tab=$_POST["tab"];
+            $ch=implode("-",$tab);
             $nom=htmlspecialchars($_POST["nom"]);
             $prenom=htmlspecialchars($_POST["prenom"]);
             $email=htmlspecialchars($_POST["email"]);
@@ -13,13 +16,14 @@
             $pdostat->execute();
             $res=$pdostat->rowcount();
             if($res==0){
-                $pdostat=$pdo->prepare("insert into information values(?,?,?,?,?,?)");
+                $pdostat=$pdo->prepare("insert into information values(?,?,?,?,?,?,?)");
                 $pdostat->bindparam(1,$nom);
                 $pdostat->bindparam(2,$prenom);
                 $pdostat->bindparam(3,$email);
                 $pdostat->bindparam(4,$mdp);
                 $pdostat->bindparam(5,$sexe);
                 $pdostat->bindparam(6,$confirmation);
+                $pdostat->bindparam(7,$ch);
                 $pdostat->execute();
                 $rep="Enregistrer avec succè !!";
             }
@@ -29,6 +33,7 @@
         }
         else{
             $rep="Veuillez remplir tous les champs et cocher les cases nécessaires !!";
+            
         }
     }
 
@@ -68,14 +73,26 @@
                 </div>
             </div>
             <div class="divrbtn">
-                <input type="radio" name="sexe" id="" class="form-check-input" value="M">
+                <input type="radio" name="sexe" id="" class="form-check-input" value="M" >
                 <label for="" >masculin</label>
                 <input type="radio" name="sexe" id="" class="form-check-input" value="F">
                 <label for="">feminin</label>
             </div>
-           
+            <label for="">langue parlée </label>
+           <div class="form-check" style="margin-bottom: 10px; margin-top:10px;">
+               <input type="checkbox" name="tab[]" class="form-check-input" value="francais" <?php if(@in_array("francais",$tab)){ echo "checked";} ?>>
+               <label for="">français</label>
+            </div>
+           <div class="form-check" style="margin-bottom: 10px;">
+                <input type="checkbox" name="tab[]"class="form-check-input" value="anglais" <?php if(@in_array("anglais",$tab)){ echo "checked";} ?>>
+                <label for="">anglais</label>
+           </div>
+           <div class="form-check" style=" margin-bottom: 10px;">
+                <input type="checkbox" name="tab[]"class="form-check-input" value="italien" <?php if(@in_array("italien",$tab)){ echo "checked";} ?>>
+                 <label for="">italien</label>
+           </div>
             <div class="form-check" style=" margin-bottom: 10px;">
-                <input type="checkbox" name="confirmation" id="" class="form-check-input" value="lorjhdgdggdgdgdgg">
+                <input type="checkbox" name="confirmation" id="" class="form-check-input" value="condition acceptées">
                 <label for="">accepter les conditions</label>
             </div>
             <button type="submit" name="envoyer"class="btn btn-primary btn-sm">Envoyer</button>
